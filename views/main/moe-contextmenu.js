c26b7f92702aea64cf98ed721cd7dd452febc62c
Menci <HuangHaoRui301@Gmail.com>
2016-08-28 23:20:22 +0800

    Added context menu

---
+++ views/main/moe-contextmenu.js
@@ -0,0 +1,57 @@
+/*
+*  This file is part of Moeditor.
+*
+*  Copyright (c) 2016 Menci <huanghaorui301@gmail.com>
+*
+*  Moeditor is free software: you can redistribute it and/or modify
+*  it under the terms of the GNU General Public License as published by
+*  the Free Software Foundation, either version 3 of the License, or
+*  (at your option) any later version.
+*
+*  Moeditor is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+*
+*  You should have received a copy of the GNU General Public License
+*  along with Moeditor. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+'use strict'
+
+document.addEventListener('DOMContentLoaded', () => {
+    const template = [
+        {
+            role: 'undo'
+        },
+        {
+            role: 'redo'
+        },
+        {
+            type: 'separator'
+        },
+        {
+            role: 'cut'
+        },
+        {
+            role: 'copy'
+        },
+        {
+            role: 'paste'
+        },
+        {
+            role: 'delete'
+        },
+        {
+            role: 'selectall'
+        }
+    ];
+
+    const remote = require('electron').remote;
+    const {Menu, MenuItem} = remote;
+    const menu = Menu.buildFromTemplate(template);
+    window.addEventListener('contextmenu', (e) => {
+        e.preventDefault();
+        menu.popup(remote.getCurrentWindow());
+    });
+});
